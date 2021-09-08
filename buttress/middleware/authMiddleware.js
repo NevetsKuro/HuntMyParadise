b7b4d943473001d510d0a07c7a6ef07fd09224a8
steven <stevenfernz96@gmail.com>
2021-09-08 23:42:31 +0530

    Feat: Husky Added

--- buttress/middleware/authMiddleware.js
+++ buttress/middleware/authMiddleware.js
@@ -1,8 +1,8 @@
 /** @format */
 
 import jwt from "jsonwebtoken";
-import User from "../models/userModel.js";
 import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
 
 const protect = asyncHandler(async (req, res, next) => {
   let token;
@@ -12,7 +12,7 @@ const protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization.startsWith("Bearer")
   ) {
     try {
-      token = req.headers.authorization.split(" ")[1];
+      [token] = req.headers.authorization.split(" ");
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
       req.user = await User.findById(decoded.id).select("-password");
--- protege/pages/Steven/index.jsx
+++ protege/pages/Steven/index.jsx
@@ -1,4 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
-export default () =>{
-  return 'Steven s Page';
-}
+
+export default () => 'Steven s Page';
