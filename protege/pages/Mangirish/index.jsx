4ffb3074e639eac39f85c83516f77ca3497aaed3
steven <stevenfernz96@gmail.com>
2021-09-22 23:43:56 +0530

    Fixed warnings

--- protege/pages/Mangirish/index.jsx
+++ protege/pages/Mangirish/index.jsx
@@ -4,7 +4,6 @@ import Image from 'next/image';
 import styles from '../../styles/Home.module.css';
 
 export default function Home() {
-  const name = 'jake';
   return (
     <div className={styles.container}>
       <Head>
