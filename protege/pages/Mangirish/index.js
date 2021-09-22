fa2004956cffdf0e3a5f0548677d9ee47581f022
steven <stevenfernz96@gmail.com>
2021-09-22 23:51:13 +0530

    Fixed formatting

--- protege/pages/Mangirish/index.js
+++ protege/pages/Mangirish/index.js
@@ -2,33 +2,29 @@ const form = document.querySelector('#form');
 const password = document.querySelector('#pass');
 const username = document.querySelector('#uname');
 
+function setError(id, error) {
+  const txtField = id.parentElement;
+  const span = txtField.querySelector('span');
+  txtField.className = 'txt_feild error';
+  span.innerText = error;
+}
+
+function validateForm() {
+  let returnVal = true;
+  const pass = password.value.trim();
+  const uname = username.value.trim();
+
+  if (uname.length < 8) {
+    setError(username, '* username too short!');
+    returnVal = false;
+  }
+
+  if (pass.length < 8) {
+    setError(password, '* password too short!');
+    returnVal = false;
+  }
+  return returnVal;
+}
 form.addEventListener('submit', () => {
-
-    validateform();
-
-})
-
-function validateform() {
-    let returnval = true;
-    const pass = password.value.trim();
-    const uname = username.value.trim();
-
-    if (uname.length < 8) {
-        seterror(username, '* username too short!');
-        returnval = false;
-    }
-
-    if (pass.length < 8) {
-        seterror(password, '* password too short!');
-        returnval = false;
-    }
-    return returnval;
-}
-function seterror(id, error) {
-
-    const txtfeild = id.parentElement;
-    const span = txtfeild.querySelector('span');
-    txtfeild.className = 'txt_feild error';
-    span.innerText = error;
-
-}
+  validateForm();
+});
