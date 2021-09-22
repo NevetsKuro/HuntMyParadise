c2309e651802aeafa5b5f336d52ec3e7ce94aec7
steven <stevenfernz96@gmail.com>
2021-09-22 23:43:30 +0530

    Fixed home css

--- protege/pages/Mangirish/index.jsx
+++ protege/pages/Mangirish/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import styles from '../../styles/Home.module.css';
 
 export default function Home() {
   const name = 'jake';
-  // eslint-disable-next-line no-console
-  console.log(name);
   return (
     <div className={styles.container}>
       <Head>
